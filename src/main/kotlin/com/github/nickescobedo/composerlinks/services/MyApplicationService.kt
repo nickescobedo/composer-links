422a871d5711027a3766a78ea003a79c570d334c
GitHub Action <action@github.com>
2021-11-03 02:06:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nickescobedo/composerlinks/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nickescobedo.composerlinks.services
+
+import com.github.nickescobedo.composerlinks.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
